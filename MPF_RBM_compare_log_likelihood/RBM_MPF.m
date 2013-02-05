0d2f6fdc36ce2776610b4358c75f1e7822ffe993
Andrew Hartnett <ahartnet@princeton.edu>
2013-02-05 14:39:23 +0100

    changed my script to do MPF learning on an RBM into a function - returns time slices of learning as well as a model struct which can be used in other code

--- MPF_RBM_compare_log_likelihood/RBM_MPF.m
+++ MPF_RBM_compare_log_likelihood/RBM_MPF.m
@@ -10,11 +10,12 @@
 % Attribution-Noncommercial License.
 % (http://creativecommons.org/licenses/by-nc/3.0/)
 
+function [model W_mpf] = RBM_MPF(d_vis, d_hid, X, n_learns)
 
 addpath ../3rd_party_code/minFunc
 
-d_vis = 24; % number of units in the visible layer
-d_hid = 50; % number of units in the hidden layer
+%d_vis = 24; % number of units in the visible layer
+%d_hid = 20; % number of units in the hidden layer
 
 
 % batch_size = 100; % number of training samples to generate
@@ -27,9 +28,9 @@ d_hid = 50; % number of units in the hidden layer
 % X = sample_RBM( Wtrue, batch_size, independent_steps, independent_steps, rand( d_vis, 1 ) > 0.5 );
 % X = X > rand(size(X));
 
-X = bint;  % I use my raw data rather than their randomly generated data
+%X = bint;  % I use my raw data rather than their randomly generated data
 
-n_learns = 5;
+%n_learns = 1;
 
 fprintf( 'initializing training weights\n' );
 Winit = randn( d_hid+1, d_vis+1 ) / sqrt(d_vis+1);
@@ -45,6 +46,10 @@ minf_options.maxFunEvals = maxlinesearch;
 minf_options.maxIter = maxlinesearch;
 Wmpf = Wmpf(:);
 
+if n_learns < 1
+    error('error - need integer number of learns')
+end
+
 %minf_options.maxiter = 1000;
 for i = 1:n_learns
     learn_t = tic();
@@ -54,4 +59,10 @@ for i = 1:n_learns
     learn_t = toc(learn_t)
     
     fprintf('learning round %i of %i took %f seconds \n',i ,n_learns, learn_t)
+end
+
+Wmpf = reshape(Wmpf, d_hid+1, d_vis+1);
+model.W = -Wmpf(1:d_hid,1:d_vis);
+model.b = -Wmpf(1:d_hid,d_vis+1)';
+model.c = -Wmpf(d_hid+1,1:d_vis);
 end
